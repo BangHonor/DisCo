b470eeb4946c042fea64928b4f8be26414ec6099
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2023-12-11 15:08:33 +0800

    Update papers100M.sh

--- scripts/papers100M.sh
+++ scripts/papers100M.sh
@@ -1,10 +1,10 @@
-# for i in 1 
-# do
-#     for r in 0.005 0.01 0.02 0.05 0.1
-#     do
-#         python -u LargeScaleCondensing_Sampled.py --dataset ogbn-papers100M --edge_pred aggr --condensing_loop 2500 --inference True  --reduction_rate=${r} --gpu_id=0 --model=SGC --seed=${i}
-#     done
-# done
+for i in 1 
+do
+    for r in 0.005 0.01 0.02 0.05 0.1
+    do
+        python -u LargeScaleCondensing_Sampled.py --dataset ogbn-papers100M --edge_pred aggr --condensing_loop 2500 --inference True  --reduction_rate=${r} --gpu_id=0 --model=SGC --seed=${i}
+    done
+done
 
 for i in 1 
 do
