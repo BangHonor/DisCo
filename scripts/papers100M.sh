a49c78069ed1626c1dbe15597bfb26d9fffbda5d
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2023-12-11 15:41:11 +0800

    Update papers100M.sh

--- scripts/papers100M.sh
+++ scripts/papers100M.sh
@@ -9,9 +9,9 @@ done
 for i in 1 
 do
     for r in 0.005 0.01 0.02 0.05 
-    do
-        python -u LargeScaleCondensing_random.py --dataset ogbn-papers100M --inference True   --method herding --reduction_rate=${r} --gpu_id=1 --model=SGC --seed=${i}
-        python -u LargeScaleCondensing_random.py --dataset ogbn-papers100M --inference True   --method kcenter --reduction_rate=${r} --gpu_id=1 --model=SGC --seed=${i}
-    
+    do   
+        python -u train_coreset_papers100M.py --dataset ogbn-papers100M --inference True   --method random --reduction_rate=${r} --gpu_id=1 --model=SGC --seed=${i}
+        python -u train_coreset_papers100M.py --dataset ogbn-papers100M --inference True   --method herding --reduction_rate=${r} --gpu_id=1 --model=SGC --seed=${i}
+        python -u train_coreset_papers100M.py --dataset ogbn-papers100M --inference True   --method kcenter --reduction_rate=${r} --gpu_id=1 --model=SGC --seed=${i}
     done
 done
