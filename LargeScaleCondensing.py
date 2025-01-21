2cae56c3160c939c32a5563bb0225da00f0d0133
Zhenbang Xiao <63777094+BangHonor@users.noreply.github.com>
2025-01-21 14:42:10 +0800

    Update LargeScaleCondensing.py

--- LargeScaleCondensing.py
+++ LargeScaleCondensing.py
@@ -27,12 +27,15 @@ from models.mlp import MLP as MLP_PYG
 from models.parametrized_adj_lp import PGE_Edge
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--gpu_id', type=int, default=0, help='gpu id')
+parser.add_argument('--gpu_id', type=int, default=2, help='gpu id')
 parser.add_argument('--parallel_gpu_ids', type=list, default=[0, 1, 2], help='gpu id')
-parser.add_argument('--dataset', type=str, default='cora')
-parser.add_argument('--seed', type=int, default=1, help='Random seed.')
+parser.add_argument('--dataset', type=str, default='ogbn-arxiv')
+parser.add_argument('--seed', type=int, default=5, help='Random seed.')
+parser.add_argument('--ablation', type=str, default='none')
+parser.add_argument('--init', type=str, default='init')
 #edge
 parser.add_argument('--edge_pred', type=str, default='aggr')
+parser.add_argument('--aggr_layer', type=int, default=1)
 parser.add_argument('--inference', type=bool, default=False)
 #gnn
 parser.add_argument('--nlayers', type=int, default=2)
@@ -46,21 +49,21 @@ parser.add_argument('--validation_model', type=str, default='MLP')
 parser.add_argument('--model', type=str, default='GCN')
 #ratio
 parser.add_argument('--keep_ratio', type=float, default=1.0)
-parser.add_argument('--reduction_rate', type=float, default=0.25)
+parser.add_argument('--reduction_rate', type=float, default=0.01)
 #condensation
 parser.add_argument('--lr_adj', type=float, default=0.01)
 parser.add_argument('--lr_feat', type=float, default=0.01)
 parser.add_argument('--lr_teacher_model', type=float, default=0.01)
-parser.add_argument('--lr_model', type=float, default=0.01)
+parser.add_argument('--lr_model', type=float, default=0.002)
 parser.add_argument('--feat_alpha', type=float, default=100)
 parser.add_argument('--dis_alpha', type=float, default=2)
 parser.add_argument('--anchor', type=int, default=1)
-parser.add_argument('--threshold', type=float, default=0.99, help='adj threshold.')
+parser.add_argument('--threshold', type=float, default=1.00, help='adj threshold.')
 parser.add_argument('--save', type=int, default=1)
 #loop and validation
 parser.add_argument('--teacher_model_loop', type=int, default=600)
 parser.add_argument('--condensing_loop', type=int, default=1500)#arxiv:1500 reddit/reddit2/products/amazon:2500 papers:5000
-parser.add_argument('--student_model_loop', type=int, default=5000)
+parser.add_argument('--student_model_loop', type=int, default=3000)
 parser.add_argument('--student_val_stage', type=int, default=100)
 args = parser.parse_args()
 print(args)
@@ -114,6 +117,26 @@ def get_ini_feat(feat_train):
     return feat_train[idx_selected]
     
 
+def get_kcenter_feat(feat_train):
+    idx_selected = []
+
+    for class_id, cnt in num_class_dict.items():
+        idx = torch.where(labels_train==class_id)[0]
+        feature = feat_train[idx]
+        mean = torch.mean(feature, dim=0, keepdim=True)
+        dis = torch.cdist(feature, mean)[:,0]
+        rank = torch.argsort(dis)
+        idx_centers = rank[:1].tolist()
+        for i in range(cnt-1):
+            feature_centers = feature[idx_centers]
+            dis_center = torch.cdist(feature, feature_centers)
+            dis_min, _ = torch.min(dis_center, dim=-1)
+            id_max = torch.argmax(dis_min).item()
+            idx_centers.append(id_max)
+        idx_selected.extend(idx[idx_centers].cpu().numpy())
+    return feat_train[idx_selected]
+
+
 def retrieve_class(c, class_dict, num=256):
     idx = np.arange(len(labels_train))
     idx = idx[class_dict['class_%s'%c]]
@@ -128,6 +151,7 @@ def link_prediction(pge_edge, nedges):
     if args.edge_pred=='aggr':
         aggr=MessagePassing(aggr="max")
         aggr_adj=SparseTensor(row=adj._indices()[0], col=adj._indices()[1], value=adj._values(), sparse_sizes=adj.size()).t().cuda()
+        
         if args.inference:
             loader=NeighborSampler(aggr_adj,
                 node_idx=torch.arange(len(labels)),
@@ -213,7 +237,7 @@ def node_condensation():
             loss = F.nll_loss(output, labels_train)
             loss.backward()
             optimizer.step()
-    torch.save(validation_model.state_dict(), f'{root}/saved_model_large/teacher/MLP_4_{args.dataset}_{args.seed}.pt')
+        torch.save(validation_model.state_dict(), f'{root}/saved_model_large/teacher/MLP_4_{args.dataset}_{args.seed}.pt')
     validation_model.load_state_dict(torch.load(f'{root}/saved_model_large/teacher/MLP_4_{args.dataset}_{args.seed}.pt'))
     output = validation_model.predict(feat_test)
     acc_test = utils.accuracy(output, labels_test)
@@ -233,13 +257,14 @@ def node_condensation():
             if coeff[c]>0:
                 feat_train_c=feat_train[index[c]]
                 feat_syn_c=feat_syn[index_syn[c]]
-                feat_loss += (coeff[c] * loss_fn(feat_train_c.mean(dim=0), feat_syn_c.mean(dim=0)))
+                if args.ablation!='class':
+                    feat_loss += (coeff[c] * loss_fn(feat_train_c.mean(dim=0), feat_syn_c.mean(dim=0)))
                 # if feat_syn_c.shape[0] > 1:
                 #     feat_loss += (coeff[c] * loss_fn(feat_train_c.std(dim=0), feat_syn_c.std(dim=0)))
-                _, I = knn_class[c].search(feat_syn_c.detach().cpu().numpy(), args.anchor)
-                I = I.ravel()
-                # dis_loss += (coeff[c] * loss_fn(feat_syn_c, feat_train_c[I]))
-                dis_loss += (coeff[c] * loss_fn(feat_syn_c, feat_train_c[I].view(feat_syn_c.shape[0], args.anchor, d).mean(dim=1)))
+                if args.ablation!='anchor':   
+                    _, I = knn_class[c].search(feat_syn_c.detach().cpu().numpy(), args.anchor)
+                    I = I.ravel()
+                    dis_loss += (coeff[c] * loss_fn(feat_syn_c, feat_train_c[I].view(feat_syn_c.shape[0], args.anchor, d).mean(dim=1)))
         feat_loss = feat_loss / coeff_sum
         dis_loss = dis_loss / coeff_sum
         loss += (args.feat_alpha * feat_loss + args.dis_alpha * dis_loss)
@@ -251,7 +276,13 @@ def node_condensation():
             acc_test = utils.accuracy(output_syn, labels_syn)
             print("Epoch", i, ", Syn Test Acc:", acc_test)
 
-    torch.save(feat_syn, f'{root}/saved_ours_large/feat_{args.dataset}_{args.anchor}_{args.reduction_rate}_{args.seed}.pt')
+    if args.ablation=='class':
+        torch.save(feat_syn, f'{root}/saved_ours_large/feat_{args.dataset}_WithoutClass_{args.anchor}_{args.reduction_rate}_{args.seed}.pt')
+    elif args.ablation=='anchor': 
+        torch.save(feat_syn, f'{root}/saved_ours_large/feat_{args.dataset}_WithoutAnchor_{args.anchor}_{args.reduction_rate}_{args.seed}.pt')
+    else:
+        torch.save(feat_syn, f'{root}/saved_ours_large/feat_{args.dataset}_{args.anchor}_{args.reduction_rate}_{args.seed}.pt')
+   
     end = time.perf_counter()
     print('Node Condensation Duration:', round(end-start), 's')
 
@@ -259,13 +290,13 @@ def node_condensation():
 def edge_construction():
     adj_syn=torch.zeros(n, n)
     if args.edge_pred=='aggr':#find the anchors，directly use their feat_transform
-        feat_syn_neighbor = torch.zeros_like(feat_syn)
+        feat_syn_neighbor = torch.zeros(feat_syn.shape[0],(args.aggr_layer)*d).cuda()
         feat_transform = torch.load(f'{root}/temp/feat_transform_aggr_max_{args.dataset}_{args.seed}.pt').cuda()[idx_train]
         neighbor = 3
         for c in range(nclass):
             if c in num_class_dict:
                 _, anchor = knn_class[c].search(feat_syn[index_syn[c]].cpu().numpy(), neighbor)
-                feat_syn_neighbor[index_syn[c]] = feat_transform[index[c]][anchor,:d].max(dim=1).values
+                feat_syn_neighbor[index_syn[c]] = feat_transform[index[c]][anchor,:(args.aggr_layer)*d].max(dim=1).values
         feat_syn_transform = torch.concat((feat_syn, feat_syn_neighbor),dim=1)
     else:
         feat_syn_transform = feat_syn
@@ -352,7 +383,7 @@ if __name__ == '__main__':
     d = feat.shape[1]
     nclass= int(labels.max()+1)
     if args.edge_pred in ['aggr']:
-        pge_edge = PGE_Edge(nfeat=2*d, device=device, args=args).cuda()
+        pge_edge = PGE_Edge(nfeat=(1+args.aggr_layer)*d, device=device, args=args).cuda()
     else:
         pge_edge = PGE_Edge(nfeat=d, device=device, args=args).cuda()
     
@@ -360,7 +391,7 @@ if __name__ == '__main__':
         if not os.path.exists(root+'/saved_ours_large/pge_aggr_max_'+args.dataset+'_'+str(args.seed)+'.pt'):
             print("Pretraining Link Prediction Model!")
             link_prediction(pge_edge, 10000)
-        pge_edge.load_state_dict(torch.load(f'{root}/saved_ours_large/pge_aggr_max_{args.dataset}_{args.seed}.pt'))
+        pge_edge.load_state_dict(torch.load(f'{root}/saved_ours_large/pge_aggr_max_{args.dataset}_{args.seed}.pt'))       
     else:
         if not os.path.exists(root+'/saved_ours_large/pge_'+args.dataset+'_'+str(args.seed)+'.pt'):
             print("Pretraining Link Prediction Model!")
@@ -371,7 +402,10 @@ if __name__ == '__main__':
     labels_syn = torch.LongTensor(labels_syn).cuda()
     n = len(labels_syn)
     feat_syn = nn.Parameter(torch.FloatTensor(n, d).cuda())
-    feat_syn.data.copy_(get_ini_feat(feat_train))
+    if args.init=='kcenter':
+        feat_syn.data.copy_(get_kcenter_feat(feat_train))
+    else:
+        feat_syn.data.copy_(get_ini_feat(feat_train))
 
     index=[]
     index_syn=[]
@@ -397,10 +431,27 @@ if __name__ == '__main__':
         else:
             knn_class.append(0)
 
-    if not os.path.exists(root+'/saved_ours_large/feat_'+args.dataset+'_'+str(args.anchor)+'_'+str(args.reduction_rate)+'_'+str(args.seed)+'.pt'):
-        print("Node Condensation!")
-        node_condensation()
-    feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda() 
+    if args.ablation=='class':
+        if not os.path.exists(root+'/saved_ours_large/feat_'+args.dataset+'_WithoutClass_'+str(args.anchor)+'_'+str(args.reduction_rate)+'_'+str(args.seed)+'.pt'):
+            print("Node Condensation!")
+            node_condensation()
+        feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_WithoutClass_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda() 
+    elif args.ablation=='anchor':
+        if not os.path.exists(root+'/saved_ours_large/feat_'+args.dataset+'_WithoutAnchor_'+str(args.anchor)+'_'+str(args.reduction_rate)+'_'+str(args.seed)+'.pt'):
+            print("Node Condensation!")
+            node_condensation()
+        feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_WithoutAnchor_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda()
+    else:
+        if not os.path.exists(root+'/saved_ours_large/feat_'+args.dataset+'_'+str(args.anchor)+'_'+str(args.reduction_rate)+'_'+str(args.seed)+'.pt'):
+            print("Node Condensation!")
+            node_condensation()
+
+    if args.ablation=='class':
+        feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_WithoutClass_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda() 
+    elif args.ablation=='anchor': 
+        feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_WithoutAnchor_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda() 
+    else:
+        feat_syn=torch.load(f'{root}/saved_ours_large/feat_{args.dataset}_{args.anchor}_{args.reduction_rate}_{args.seed}.pt', map_location="cpu").detach().cuda() 
 
     #edge construction
     edge_index_syn, edge_weight_syn = edge_construction()
